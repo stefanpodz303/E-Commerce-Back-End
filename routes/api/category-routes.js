9abae2cb43799a84df04bd54bcd49e9b3a3070e1
The Podz <stefanpodz@yahoo.com>
2021-04-23 09:27:44 -0400

    added tag-routes code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,7 +59,7 @@ router.put('/:id', async (req, res) => {
   }
   });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
